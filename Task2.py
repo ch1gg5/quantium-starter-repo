438996d16a25b0fcba84a6ee1086a83455fefe24
ch1gg5 <danieldera06@gmail.com>
2026-06-12 21:19:19 +0100

    task 2 done

---
+++ Task2.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+#initialise and assign main df and temp df
+df = pd.read_csv("data/daily_sales_data_0.csv")
+
+#assign the 2nd csv file to temp df and concat it to the main one
+tempdf = pd.read_csv("data/daily_sales_data_1.csv")
+df = pd.concat([df, tempdf], ignore_index=True)
+
+#do the same for the 3rd csv file
+tempdf = pd.read_csv("data/daily_sales_data_2.csv")
+df = pd.concat([df, tempdf], ignore_index=True)
+
+#now remove any product that isnt pink morsel
+df = df[df["product"] == "pink morsel"]
+
+#new column called sales
+df["sales"] = (df["price"].str.replace("$", "").astype(float) * df["quantity"]).apply(lambda x: f"${x:.2f}")
+
+#remove  column price and quantity
+df = df.drop(columns=["price", "quantity", "product"])
+#resetting the index
+df = df.reset_index(drop=True)
+
